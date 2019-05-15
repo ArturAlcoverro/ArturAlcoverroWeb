8d2a483efbcb099e4cf6912011e1c6c3f5818493
ElTitoBadi <artur.bcn1998@gmail.com>
2019-05-16 00:25:42 +0200

    -

--- public/js/project.js
+++ public/js/project.js
@@ -1,25 +1,23 @@
-$(document).ready(function(){
-    $('.project').hover(function(){
+$(document).ready(function () {
+    $('.project').hover(function () {
         var $parent = $(this).parent().parent();
         var name = $(this).data('name');
-        var color =  $(this).data('color')
+        var color = $(this).data('color')
         $parent.addClass('bg-navbar-' + name);
-        setTimeout(() => {
-            $('.animation-'+name).addClass('animation-'+name+'-on');
-        }, 100);
+        $('.animation-' + name).addClass('animation-' + name + '-on');
         $(this).parent().children().children().addClass('opacity-0');
         $(this).parent().children().addClass('bg-transparent');
         $(this).children().removeClass('opacity-0');
         $(this).removeClass('bg-transparent');
 
-        if($('#work').offset().top <= (document.documentElement.scrollTop + 66)){
+        if ($('#work').offset().top <= (document.documentElement.scrollTop + 66)) {
             $('#navbar').addClass('bg-navbar-' + name);
         }
-    },function(){
+    }, function () {
         var $parent = $(this).parent().parent();
         var name = $(this).data('name');
         $parent.removeClass('bg-navbar-' + name);
-        $('.animation-'+name).removeClass('animation-'+name+'-on');
+        $('.animation-' + name).removeClass('animation-' + name + '-on');
         $(this).parent().children().children().removeClass('opacity-0');
         $(this).parent().children().removeClass('bg-transparent');
         $('#navbar').removeClass('bg-navbar-' + name);
