1f6b7a16c645203befde681bde4eeca6370be3e9
Artur Alcoverro <artur.bcn1998@gmail.com>
2020-02-03 22:12:26 +0100

    -

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Arturr Alcoverro</title>
+    <title>Arturrr Alcoverro</title>
     <link rel="shortcut icon" type="image/png" href="{{asset('/assets/imgs/favicon.png')}}">
     <link rel="stylesheet" href="{{asset('/css/bootstrap/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('/css/menu-animation.css')}}">
