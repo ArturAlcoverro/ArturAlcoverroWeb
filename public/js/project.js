5927d554944b19622c9a4bc4831b273478f3ea7d
Artur Alcoverro Pastó <artur.alcoverro@students.salle.url.edu>
2020-06-07 18:18:12 +0200

    -

--- public/js/project.js
+++ public/js/project.js
@@ -28,37 +28,7 @@ $(document).ready(function () {
         $(this).parent().children().removeClass('bg-transparent');
         $('#navbar').removeClass('bg-' + name);
         svgAnimationOff(name);
-    })
-
-    $('.project a').on('focus', function () {
-        $this = $(this).parent();
-        var $parent = $this.parent().parent();
-        var name = $this.data('name');
-        var color = $this.data('color')
-        $parent.addClass('bg-navbar-' + name);
-        $('.animation-' + name).addClass('animation-' + name + '-on');
-        $this.parent().children().children().addClass('opacity-0');
-        $this.parent().children().addClass('bg-transparent');
-        $this.children().removeClass('opacity-0');
-        $this.removeClass('bg-transparent');
-        svgAnimation(name);
-
-        if ($('#work').offset().top <= (document.documentElement.scrollTop + 66)) {
-            $('#navbar').addClass('bg-' + name);
-        }
-    })
-
-    $('.project a').on('blur', function () {
-        $this = $(this).parent();
-        var $parent = $this.parent().parent();
-        var name = $this.data('name');
-        $parent.removeClass('bg-navbar-' + name);
-        $('.animation-' + name).removeClass('animation-' + name + '-on');
-        $this.parent().children().children().removeClass('opacity-0');
-        $this.parent().children().removeClass('bg-transparent');
-        $('#navbar').removeClass('bg-' + name);
-        svgAnimationOff(name);
-    })
+    });
 });
 
 function svgAnimation(name) {
